089dc60a4a5cdbb807067c40b04a5e8c25577f47
ajaykontham <ajayk91@gmail.com>
2023-12-18 23:27:07 -0500

    Updated base 64 encode

--- pyBase64EncDecGUI.py
+++ pyBase64EncDecGUI.py
@@ -8,6 +8,13 @@ def encode_base64():
     output_text.delete('1.0', tk.END)
     output_text.insert(tk.END, encoded)
 
+def encode_base64_CS():
+    input_text_clientId = input_entry_clientId.get()
+    input_text_clientSec= input_entry_clientSec.get()
+    encoded = base64.b64encode((input_text_clientId +":"+input_text_clientSec).encode()).decode()
+    output_textCS.delete('1.0', tk.END)
+    output_textCS.insert(tk.END, encoded)
+
 # Function to decode
 def decode_base64():
     input_text = input_entry.get()
@@ -36,6 +43,24 @@ decode_button.grid(row=1, column=1)
 
 output_text = tk.Text(root, height=5, width=50)
 output_text.grid(row=2, column=0, columnspan=2)
+
+
+tk.Label(root, text="ClientId:").grid(row=3, column=0)
+input_entry_clientId = tk.Entry(root, width=50)
+input_entry_clientId.grid(row=3, column=1)
+
+tk.Label(root, text="ClientSecret:").grid(row=4, column=0)
+input_entry_clientSec = tk.Entry(root, width=50)
+input_entry_clientSec.grid(row=4, column=1)
+
+encode_buttonCS = tk.Button(root, text="Encode", command=encode_base64_CS)
+encode_buttonCS.grid(row=5, column=1)
+
+# decode_button = tk.Button(root, text="Decode", command=decode_base64)
+# decode_button.grid(row=1, column=1)
+
+output_textCS = tk.Text(root, height=5, width=50)
+output_textCS.grid(row=6, column=0, columnspan=2)
 
 # Run the application
 root.mainloop()
